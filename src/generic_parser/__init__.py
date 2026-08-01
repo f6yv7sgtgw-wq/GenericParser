d59a3653fc8fa9f4dd7b8ebbf62e67ba0e6357a2
f6yv7sgtgw-wq <f6yv7sgtgw@privaterelay.appleid.com>
2026-08-01 11:43:43 +0200

    Expose version 0.2.0rc2

--- src/generic_parser/__init__.py
+++ src/generic_parser/__init__.py
@@ -1,74 +1,20 @@
 """Öffentliche API des GenericParser-Pakets."""
 
-from .config import (
-    ConfigurationError,
-    load_profile,
-    load_profiles,
-    profile_from_dict,
-    profile_to_dict,
-    save_profile,
-    save_profiles,
-)
-from .models import (
-    Listing,
-    Location,
-    MatchDecision,
-    MatchResult,
-    NormalizedPrice,
-    PriceFlag,
-    SearchProfile,
-)
+from .config import ConfigurationError, load_profile, load_profiles, profile_from_dict, profile_to_dict, save_profile, save_profiles
+from .models import Listing, Location, MatchDecision, MatchResult, NormalizedPrice, PriceFlag, SearchProfile
 from .normalization import compact_text, normalize_text, parse_location, parse_posted_at, parse_price
 from .service import GenericParser, ListingSource
-from .sources.kleinanzeigen import (
-    FetchedPage,
-    KleinanzeigenAdapter,
-    KleinanzeigenBlockedError,
-    KleinanzeigenHttpClient,
-    KleinanzeigenLayoutError,
-    LocationVerification,
-    KleinanzeigenPageParser,
-    KleinanzeigenUrlBuilder,
-    PageDiagnostics,
-    PageState,
-    extract_location_id,
-    slugify_keyword,
-)
+from .sources.kleinanzeigen import FetchedPage, KleinanzeigenAdapter, KleinanzeigenBlockedError, KleinanzeigenHttpClient, KleinanzeigenLayoutError, LocationVerification, KleinanzeigenPageParser, KleinanzeigenUrlBuilder, PageDiagnostics, PageState, extract_location_id, slugify_keyword
 
-__version__ = "0.2.0rc1"
+__version__ = "0.2.0rc2"
 
 __all__ = [
-    "ConfigurationError",
-    "GenericParser",
-    "FetchedPage",
-    "KleinanzeigenAdapter",
-    "KleinanzeigenBlockedError",
-    "KleinanzeigenHttpClient",
-    "KleinanzeigenLayoutError",
-    "LocationVerification",
-    "KleinanzeigenPageParser",
-    "KleinanzeigenUrlBuilder",
-    "PageDiagnostics",
-    "PageState",
-    "Listing",
-    "ListingSource",
-    "Location",
-    "MatchDecision",
-    "MatchResult",
-    "NormalizedPrice",
-    "PriceFlag",
-    "SearchProfile",
-    "compact_text",
-    "load_profile",
-    "load_profiles",
-    "normalize_text",
-    "parse_location",
-    "parse_posted_at",
-    "parse_price",
-    "profile_from_dict",
-    "profile_to_dict",
-    "save_profile",
-    "save_profiles",
-    "extract_location_id",
-    "slugify_keyword",
+    "ConfigurationError", "GenericParser", "FetchedPage", "KleinanzeigenAdapter",
+    "KleinanzeigenBlockedError", "KleinanzeigenHttpClient", "KleinanzeigenLayoutError",
+    "LocationVerification", "KleinanzeigenPageParser", "KleinanzeigenUrlBuilder",
+    "PageDiagnostics", "PageState", "Listing", "ListingSource", "Location",
+    "MatchDecision", "MatchResult", "NormalizedPrice", "PriceFlag", "SearchProfile",
+    "compact_text", "load_profile", "load_profiles", "normalize_text", "parse_location",
+    "parse_posted_at", "parse_price", "profile_from_dict", "profile_to_dict",
+    "save_profile", "save_profiles", "extract_location_id", "slugify_keyword",
 ]
