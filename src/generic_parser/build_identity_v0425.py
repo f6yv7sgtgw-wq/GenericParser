18dc0e56116df53d428d306e41fd6197bd9b3647
f6yv7sgtgw-wq <f6yv7sgtgw@privaterelay.appleid.com>
2026-08-02 23:10:48 +0200

    Add shared 0.42.5 build identity

---
+++ src/generic_parser/build_identity_v0425.py
@@ -0,0 +1,6 @@
+"""Single build identity for GenericParser 0.42.5."""
+VERSION = "0.42.5"
+BUILD_ID = "gp-0425-20260802-1"
+BUILD_REVISION = BUILD_ID
+API_CONTRACT = "match-v6.1-page-worker"
+EVENT_LOG_KEY = "generic-parser-eventlog-0425"
