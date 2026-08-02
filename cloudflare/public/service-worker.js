48dd6e96f83fdc87d856a08a5d1b0da6658df851
f6yv7sgtgw-wq <f6yv7sgtgw@privaterelay.appleid.com>
2026-08-02 09:09:40 +0200

    Bump service worker cache for 0.34

--- cloudflare/public/service-worker.js
+++ cloudflare/public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="generic-parser-mobile-0.33";
-const ASSETS=["./","./app.css?v=0.33","./app.js?v=0.33","./manifest.webmanifest?v=0.33","./icons/icon.svg"];
+const CACHE="generic-parser-mobile-0.34";
+const ASSETS=["./","./app.css?v=0.34","./app.js?v=0.34","./manifest.webmanifest?v=0.34","./icons/icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
