4941ebcf6f5fa1f9c48e9971108ad29658f6244f
f6yv7sgtgw-wq <f6yv7sgtgw@privaterelay.appleid.com>
2026-08-02 09:51:27 +0200

    Refresh service worker cache for 0.351

--- cloudflare/public/service-worker.js
+++ cloudflare/public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="generic-parser-mobile-0.35";
-const ASSETS=["./","./app.css","./app.js","./manifest.webmanifest","./icons/icon.svg"];
+const CACHE="generic-parser-mobile-0.351";
+const ASSETS=["./","./app.css?v=0.351","./app.js?v=0.351","./manifest.webmanifest?v=0.351","./icons/icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
