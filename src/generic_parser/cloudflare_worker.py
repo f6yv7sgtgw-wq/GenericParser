52844c08946114bfcc6acf3dd3c13990e9908e55
f6yv7sgtgw-wq <f6yv7sgtgw@privaterelay.appleid.com>
2026-08-02 10:37:10 +0200

    Bump Cloudflare worker entrypoint to 0.352

--- src/generic_parser/cloudflare_worker.py
+++ src/generic_parser/cloudflare_worker.py
@@ -1,4 +1,4 @@
-"""Cloudflare-Python-Worker-Einstiegspunkt für GenericParser 0.351."""
+"""Cloudflare-Python-Worker-Einstiegspunkt für GenericParser 0.352."""
 
 from __future__ import annotations
 
